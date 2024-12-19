13ae795e5a5d1ffb7d6c8e5b8c8102ed00aafb1b
eriksun0310 <113155520+eriksun0310@users.noreply.github.com>
2024-12-19 14:47:03 +0800

    setup expo notifications

---
+++ usePushNotifications.ts
@@ -0,0 +1,63 @@
+import { useState, useEffect, useRef } from "react";
+
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+
+import Constants from "expo-constants";
+
+import { Platform } from "react-native";
+import { registerForPushNotificationsAsync } from "./util/registerForPushNotificationsAsync";
+
+export interface PushNotificationState {
+  notification?: Notifications.Notification;
+  expoPushToken?: Notifications.ExpoPushToken;
+}
+
+export const usePushNotifications = (): PushNotificationState => {
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldPlaySound: false,
+      shouldShowAlert: false,
+      shouldSetBadge: false,
+    }),
+  });
+
+  const [expoPushToken, setExpoPushToken] = useState<"">;
+
+  const [notification, setNotification] = useState<
+    Notifications.Notification | undefined
+  >();
+
+  const [] = useState<Error | null>(null);
+
+  const notificationListener = useRef<Notifications.EventSubscription>();
+  const responseListener = useRef<Notifications.EventSubscription>();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+      .then((token) => setExpoPushToken(token ?? ""))
+      .catch((error: any) => setExpoPushToken(`${error}`));
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        console.log("notification", notification);
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      notificationListener.current &&
+        Notifications.removeNotificationSubscription(
+          notificationListener.current
+        );
+      responseListener.current &&
+        Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  return { notification, expoPushToken };
+};
